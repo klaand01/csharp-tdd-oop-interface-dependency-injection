919e3f979c2c6752cdabc945566cd7ac1a9ffc03
Klara Andersson <klara.andersson@se.experis.com>
2024-01-17 12:02:02 +0100

    Core made

--- csharp-tdd-oop-interface-dependency-injection.Tests/ScrabbleTest.cs
+++ csharp-tdd-oop-interface-dependency-injection.Tests/ScrabbleTest.cs
@@ -8,49 +8,57 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Test
         public class ScrabbleTest {
         [Test]
         public void shouldGive0ForEmptyWords() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(0, scrabble.score(""));
         }
 
         [Test]
         public void shouldGive0ForWhiteSpace() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(0, scrabble.score("\n\r\t\b\f"));
         }
 
         [Test]
         public void shouldScore1ForA() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(1, scrabble.score("a"));
         }
 
         [Test]
         public void shouldScore4ForF() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(4, scrabble.score("f"));
         }
 
         [Test]
         public void shouldScore6ForStreet() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(6, scrabble.score("street"));
         }
 
         [Test]
         public void shouldScore22ForQuirky() {
-            Scrabble scrabble = new Scrabble();
+            EnglishAlphabet english = new EnglishAlphabet();
+            Scrabble scrabble = new Scrabble(english);
             Assert.AreEqual(22, scrabble.score("quirky"));
         }
 
         [Test]
         public void shouldScoreRussianLetters() {
-            Scrabble scrabble = new Scrabble();
+            RussianAlphabet russian = new RussianAlphabet();
+            Scrabble scrabble = new Scrabble(russian);
             Assert.AreEqual(18, scrabble.score("дврфъ"));
         }
 
         [Test]
         public void shouldScoreGreekLetters() {
-            Scrabble scrabble = new Scrabble();
+            GreekAlphabet greek = new GreekAlphabet();
+            Scrabble scrabble = new Scrabble(greek);
             Assert.AreEqual(20, scrabble.score("φεψωλ"));
         }
     }
--- tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
@@ -9,9 +9,8 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Main
     public class Scrabble {
         private Dictionary<Char, int> letterScores;
 
-        public Scrabble() {
-            Alphabet a = new Alphabet();
-            this.letterScores = a.getLetterScores();
+        public Scrabble(Alphabet alphabet) {
+            this.letterScores = alphabet.getLetterScores();
         }
 
         public int score(String word) {
